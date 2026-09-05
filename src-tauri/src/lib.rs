370dcbadc152f357f94d639cedb42274742507ad
fiqri1010 <afiqri22@gmail.com>
2026-09-05 16:20:24 +0700

    chore: bump version patch

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,16 @@
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        // Inisialisasi Plugin Single Instance
+        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
-  app_lib::run();
+    tauri::Builder::default()
+        // Inisialisasi Plugin Single Instance: Mencegah aplikasi dibuka lebih dari 1 kali
+        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+            // Jika pengguna mencoba membuka aplikasi lagi, fokuskan jendela yang sudah aktif
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
