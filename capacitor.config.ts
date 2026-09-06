5230e81a20f4bd8a001694d55e8f1adbfe6ad61d
fiqri1010 <afiqri22@gmail.com>
2026-09-07 01:14:41 +0700

    Inisiasi file dan folder baru pembuatan program android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jadwalpriok.app',
+  appName: 'Jadwal Priok',
+  webDir: 'dist'
+};
+
+export default config;
